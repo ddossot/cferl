45b897a47b0de5ea7c5660b6ce59e90b56cc20b5
Tilman Holschuh <tholschuh@pugpharm.com>
2013-03-14 20:03:44 -0700

    removed paramterized modules

--- include/cferl.hrl
+++ include/cferl.hrl
@@ -6,19 +6,41 @@
 %%% Copyright (c) 2010 David Dossot
 %%%
 
--define(API_BASE_URL, "auth.api.rackspacecloud.com").
+-define(US_API_BASE_URL, "identity.api.rackspacecloud.com").
+-define(UK_API_BASE_URL, "lon.identity.api.rackspacecloud.com").
 -define(VERSION_PATH, "/v1.0").
 
 -define(DEFAULT_REQUEST_TIMEOUT, 30000).
 -define(OBJECT_META_HEADER_PREFIX, "X-Object-Meta-").
 -define(DIRECTORY_OBJECT_CONTENT_TYPE, <<"application/directory">>).
 
+-define(IS_CONNECTION(C), is_record(C, cf_connection)).
+-define(IS_CONTAINER(C), is_record(C, cf_container)).
+-define(IS_OBJECT(O), is_record(O, cf_object)).
+
+-record(cf_connection, {version :: string(), 
+                        auth_token :: string(), 
+                        storage_url :: string(), 
+                        cdn_management_url :: string()
+                       }).
+
 -record(cf_account_info, {bytes_used, container_count}).
 
--record(cf_container_query_args, {marker, limit}).
 -record(cf_container_details, {name, bytes, count}).
+-record(cf_container, {container_details :: #cf_container_details{},
+                       container_path :: string(), 
+                       cdn_details :: [{atom(), term()}]
+                      }).
+
+-record(cf_container_query_args, {marker, limit}).
 -record(cf_container_cdn_config, {ttl = 86400, user_agent_acl, referrer_acl}).
 
--record(cf_object_query_args, {marker, limit, prefix, path}).
 -record(cf_object_details, {name, bytes = 0, last_modified, content_type, etag}).
+-record(cf_object, {container :: #cf_container{}, 
+                    object_details :: #cf_object_details{}, 
+                    object_path :: string(), 
+                    http_headers :: [{string(), string()}]
+                   }).
+
+-record(cf_object_query_args, {marker, limit, prefix, path}).
 
--- test/cferl_integration_tests.erl
+++ test/cferl_integration_tests.erl
@@ -45,33 +45,33 @@ connect_test(Username, ApiKey) ->
 
 print_account_info(CloudFiles) ->
   ?PRINT_CODE("# Retrieve the account information record"),
-  ?PRINT_CALL({ok, Info} = CloudFiles:get_account_info()),
+  ?PRINT_CALL({ok, Info} = cferl_connection:get_account_info(CloudFiles)),
   ?PRTFM_CODE("Info = #cf_account_info{bytes_used=~B, container_count=~B}",
               [Info#cf_account_info.bytes_used, Info#cf_account_info.container_count]),
   ?PRINT_CODE("").
               
 container_tests(CloudFiles) ->
   ?PRINT_CODE("# Retrieve names of all existing containers (within the limits imposed by Cloud Files server)"),
-  ?PRINT_CALL({ok, Names} = CloudFiles:get_containers_names()),
+  ?PRINT_CALL({ok, Names} = cferl_connection:get_containers_names(CloudFiles)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Retrieve names of a maximum of 3 existing containers"),
-  ?PRINT_CALL({ok, ThreeNamesMax} = CloudFiles:get_containers_names(#cf_container_query_args{limit=3})),
+  ?PRINT_CALL({ok, ThreeNamesMax} = cferl_connection:get_containers_names(CloudFiles, #cf_container_query_args{limit=3})),
   ?PRINT_CODE(""),
   
   % retrieve 0 container
-  {ok, []} = CloudFiles:get_containers_details(#cf_container_query_args{limit=0}),
+  {ok, []} = cferl_connection:get_containers_details(CloudFiles, #cf_container_query_args{limit=0}),
   
   ?PRINT_CODE("# Retrieve names of all containers currently CDN activated"),
-  ?PRINT_CALL({ok, CurrentPublicNames} = CloudFiles:get_public_containers_names(active)),
+  ?PRINT_CALL({ok, CurrentPublicNames} = cferl_connection:get_public_containers_names(CloudFiles, active)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Retrieve names of all containers that are currently or have been CDN activated"),
-  ?PRINT_CALL({ok, AllTimePublicNames} = CloudFiles:get_public_containers_names(all_time)),
+  ?PRINT_CALL({ok, AllTimePublicNames} = cferl_connection:get_public_containers_names(CloudFiles, all_time)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Retrieve details for all existing containers (within the server limits)"),
-  ?PRINT_CALL({ok, ContainersDetails} = CloudFiles:get_containers_details()),
+  ?PRINT_CALL({ok, ContainersDetails} = cferl_connection:get_containers_details(CloudFiles)),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# ContainersDetails is a list of #cf_container_details records"),
@@ -83,18 +83,18 @@ container_tests(CloudFiles) ->
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Retrieve details for a maximum of 5 containers whose names start at cf"),
-  ?PRINT_CALL({ok, CfContainersDetails} = CloudFiles:get_containers_details(#cf_container_query_args{marker= <<"cf">>, limit=5})),
+  ?PRINT_CALL({ok, CfContainersDetails} = cferl_connection:get_containers_details(CloudFiles, #cf_container_query_args{marker= <<"cf">>, limit=5})),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Get a container reference by name"),
-  ?PRINT_CALL({ok, Container} = CloudFiles:get_container(ContainerDetails#cf_container_details.name)),
+  ?PRINT_CALL({ok, Container} = cferl_connection:get_container(CloudFiles, ContainerDetails#cf_container_details.name)),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# Get container details from its reference"),
-  ?PRINT_CALL(ContainerName = Container:name()),
-  ?PRINT_CALL(ContainerBytes = Container:bytes()),
-  ?PRINT_CALL(ContainerSize = Container:count()),
-  ?PRINT_CALL(ContainerIsEmpty = Container:is_empty()),
+  ?PRINT_CALL(ContainerName = cferl_container:name(Container)),
+  ?PRINT_CALL(ContainerBytes = cferl_container:bytes(Container)),
+  ?PRINT_CALL(ContainerSize = cferl_container:count(Container)),
+  ?PRINT_CALL(ContainerIsEmpty = cferl_container:is_empty(Container)),
   ?PRINT_CODE(""),
   ?PRTFM_CODE("# -> Name: ~p - Bytes: ~p - Size: ~p - IsEmpty: ~p",
               [ContainerName, ContainerBytes, ContainerSize, ContainerIsEmpty]),
@@ -103,82 +103,82 @@ container_tests(CloudFiles) ->
   NewContainerName = make_new_container_name(),
   
   ?PRINT_CODE("# Check a container's existence"),
-  ?PRINT_CALL(false = CloudFiles:container_exists(NewContainerName)),
+  ?PRINT_CALL(false = cferl_connection:container_exists(CloudFiles, NewContainerName)),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# Create a new container"),
-  ?PRINT_CALL({ok, NewContainer} = CloudFiles:create_container(NewContainerName)),
+  ?PRINT_CALL({ok, NewContainer} = cferl_connection:create_container(CloudFiles, NewContainerName)),
   ?PRINT_CODE(""),
-  ?PRINT_CALL(true = CloudFiles:container_exists(NewContainerName)),
+  ?PRINT_CALL(true = cferl_connection:container_exists(CloudFiles, NewContainerName)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("Check attributes of this newly created container"),
-  ?PRINT_CALL(NewContainerName = NewContainer:name()),
-  ?PRINT_CALL(0 = NewContainer:bytes()),
-  ?PRINT_CALL(0 = NewContainer:count()),
-  ?PRINT_CALL(true = NewContainer:is_empty()),
-  ?PRINT_CALL(false = NewContainer:is_public()),
-  ?PRINT_CALL(<<>> = NewContainer:cdn_url()),
-  ?PRINT_CALL(0 = NewContainer:cdn_ttl()),
-  ?PRINT_CALL(false = NewContainer:log_retention()),
+  ?PRINT_CALL(NewContainerName = cferl_container:name(NewContainer)),
+  ?PRINT_CALL(0 = cferl_container:bytes(NewContainer)),
+  ?PRINT_CALL(0 = cferl_container:count(NewContainer)),
+  ?PRINT_CALL(true = cferl_container:is_empty(NewContainer)),
+  ?PRINT_CALL(false = cferl_container:is_public(NewContainer)),
+  ?PRINT_CALL(<<>> = cferl_container:cdn_url(NewContainer)),
+  ?PRINT_CALL(0 = cferl_container:cdn_ttl(NewContainer)),
+  ?PRINT_CALL(false = cferl_container:log_retention(NewContainer)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Make the container public on the CDN (using the default TTL and ACLs)"),
-  ?PRINT_CALL(ok = NewContainer:make_public()),
+  ?PRINT_CALL(ok = cferl_container:make_public(CloudFiles, NewContainer)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Activate log retention on the new container"),
-  ?PRINT_CALL(ok = NewContainer:set_log_retention(true)),
+  ?PRINT_CALL(ok = cferl_container:set_log_retention(CloudFiles, NewContainer, true)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Refresh an existing container and check its attributes"),
-  ?PRINT_CALL({ok, RefreshedContainer} = NewContainer:refresh()),
-  ?PRINT_CALL(true = RefreshedContainer:is_public()),
+  ?PRINT_CALL({ok, RefreshedContainer} = cferl_container:refresh(CloudFiles, NewContainer)),
+  ?PRINT_CALL(true = cferl_container:is_public(RefreshedContainer)),
   ?PRINT_CODE(""),
-  ?PRINT_CALL(io:format("    ~s~n~n", [RefreshedContainer:cdn_url()])),
-  ?PRINT_CALL(86400 = RefreshedContainer:cdn_ttl()),
-  ?PRINT_CALL(true = RefreshedContainer:log_retention()),
+  ?PRINT_CALL(io:format("    ~s~n~n", [cferl_container:cdn_url(RefreshedContainer)])),
+  ?PRINT_CALL(86400 = cferl_container:cdn_ttl(RefreshedContainer)),
+  ?PRINT_CALL(true = cferl_container:log_retention(RefreshedContainer)),
   ?PRINT_CODE(""),
   
   % ensure container has no object name
-  {ok, []} = RefreshedContainer:get_objects_names(),
-  {ok, []} = RefreshedContainer:get_objects_names(#cf_object_query_args{limit=10}),
+  {ok, []} = cferl_container:get_objects_names(CloudFiles, RefreshedContainer),
+  {ok, []} = cferl_container:get_objects_names(CloudFiles, RefreshedContainer, #cf_object_query_args{limit=10}),
 
   % ensure container has no object details
-  {ok, []} = RefreshedContainer:get_objects_details(),
-  {ok, []} = RefreshedContainer:get_objects_details(#cf_object_query_args{limit=10}),
+  {ok, []} = cferl_container:get_objects_details(CloudFiles, RefreshedContainer),
+  {ok, []} = cferl_container:get_objects_details(CloudFiles, RefreshedContainer, #cf_object_query_args{limit=10}),
   
   ?PRINT_CALL(ObjectName = <<"test.xml">>),
   
   % ensure new object doesn't exist
-  false = RefreshedContainer:object_exists(ObjectName),
+  false = cferl_container:object_exists(CloudFiles, RefreshedContainer, ObjectName),
   
   ?PRINT_CODE("# Create an object *reference*, nothing is sent to the server yet"),
-  ?PRINT_CALL({ok, Object} = RefreshedContainer:create_object(ObjectName)),
+  ?PRINT_CALL({ok, Object} = cferl_container:create_object(CloudFiles, RefreshedContainer, ObjectName)),
   ?PRINT_CODE("# As expected, it doesn't exist yet"),
-  ?PRINT_CALL(false = RefreshedContainer:object_exists(ObjectName)),
+  ?PRINT_CALL(false = cferl_container:object_exists(CloudFiles, RefreshedContainer, ObjectName)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Write data in the object, which creates it on the server"),
-  ?PRINT_CALL(ok = Object:write_data(<<"<test />">>, <<"application/xml">>)),
+  ?PRINT_CALL(ok = cferl_object:write_data(CloudFiles, Object, <<"<test />">>, <<"application/xml">>)),
   ?PRINT_CODE("# Now it exists!"),
-  ?PRINT_CALL(true = RefreshedContainer:object_exists(ObjectName)),
+  ?PRINT_CALL(true = cferl_container:object_exists(CloudFiles, RefreshedContainer, ObjectName)),
   ?PRINT_CODE("# And trying to re-create it just returns it"),
-  ?PRINT_CALL({ok, ExistingObject} = RefreshedContainer:create_object(ObjectName)),
+  ?PRINT_CALL({ok, ExistingObject} = cferl_container:create_object(CloudFiles, RefreshedContainer, ObjectName)),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# Set custom meta-data on it"),
-  ?PRINT_CALL(ok = Object:set_metadata([{<<"Key123">>, <<"my 123 Value">>}])),
+  ?PRINT_CALL(ok = cferl_object:set_metadata(CloudFiles, Object, [{<<"Key123">>, <<"my 123 Value">>}])),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# An existing object can be accessed directly from its container"),
-  ?PRINT_CALL({ok, GotObject} = RefreshedContainer:get_object(ObjectName)),
+  ?PRINT_CALL({ok, GotObject} = cferl_container:get_object(CloudFiles, RefreshedContainer, ObjectName)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Object names and details can be queried"),
-  ?PRINT_CALL({ok, [ObjectName]} = RefreshedContainer:get_objects_names()),
-  ?PRINT_CALL({ok, [ObjectName]} = RefreshedContainer:get_objects_names(#cf_object_query_args{limit=1})),
-  ?PRINT_CALL({ok, [ObjectDetails]} = RefreshedContainer:get_objects_details()),
+  ?PRINT_CALL({ok, [ObjectName]} = cferl_container:get_objects_names(CloudFiles, RefreshedContainer)),
+  ?PRINT_CALL({ok, [ObjectName]} = cferl_container:get_objects_names(CloudFiles, RefreshedContainer, #cf_object_query_args{limit=1})),
+  ?PRINT_CALL({ok, [ObjectDetails]} = cferl_container:get_objects_details(CloudFiles, RefreshedContainer)),
   ?PRTFM_CODE("ObjectDetails = #cf_object_details{name=~p, bytes=~B, last_modified=~1024p, content_type=~s, etag=~s}",
               [ObjectDetails#cf_object_details.name,
                ObjectDetails#cf_object_details.bytes,
@@ -188,33 +188,33 @@ container_tests(CloudFiles) ->
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# Read the whole data"),
-  ?PRINT_CALL({ok, <<"<test />">>} = Object:read_data()),
+  ?PRINT_CALL({ok, <<"<test />">>} = cferl_object:read_data(CloudFiles, Object)),
   ?PRINT_CODE("# Read the data with an offset and a size"),
-  ?PRINT_CALL({ok, <<"test">>} = Object:read_data(1, 4)),
+  ?PRINT_CALL({ok, <<"test">>} = cferl_object:read_data(CloudFiles, Object, 1, 4)),
   ?PRINT_CODE(""),
 
   ?PRINT_CODE("# Refresh the object so its attributes and metadata are up to date"),
-  ?PRINT_CALL({ok, RefreshedObject} = Object:refresh()),
+  ?PRINT_CALL({ok, RefreshedObject} = cferl_object:refresh(CloudFiles, Object)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Get object attributes"),
-  ?PRINT_CALL(ObjectName = RefreshedObject:name()),
-  ?PRINT_CALL(8 = RefreshedObject:bytes()),
-  ?PRINT_CALL({{D,M,Y},{H,Mi,S}} = RefreshedObject:last_modified()),
-  ?PRINT_CALL(<<"application/xml; charset=UTF-8">> = RefreshedObject:content_type()),
-  ?PRINT_CALL(Etag = RefreshedObject:etag()),
+  ?PRINT_CALL(ObjectName = cferl_object:name(RefreshedObject)),
+  ?PRINT_CALL(8 = cferl_object:bytes(RefreshedObject)),
+  ?PRINT_CALL({{D,M,Y},{H,Mi,S}} = cferl_object:last_modified(RefreshedObject)),
+  ?PRINT_CALL(<<"application/xml">> = cferl_object:content_type(RefreshedObject)),
+  ?PRINT_CALL(Etag = cferl_object:etag(RefreshedObject)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Get custom meta-data"),
-  ?PRINT_CALL([{<<"Key123">>, <<"my 123 Value">>}] = RefreshedObject:metadata()),
+  ?PRINT_CALL([{<<"Key123">>, <<"my 123 Value">>}] = cferl_object:metadata(RefreshedObject)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Delete the object"),
-  ?PRINT_CALL(ok = RefreshedObject:delete()),
+  ?PRINT_CALL(ok = cferl_object:delete(CloudFiles, RefreshedObject)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Data can be streamed to the server from a generating function"),
-  ?PRINT_CALL({ok, StreamedObject} = RefreshedContainer:create_object(<<"streamed.txt">>)),
+  ?PRINT_CALL({ok, StreamedObject} = cferl_container:create_object(CloudFiles, RefreshedContainer, <<"streamed.txt">>)),
 
   DataPid = spawn_data_producer(),
   WriteDataFun =
@@ -226,40 +226,40 @@ container_tests(CloudFiles) ->
       end
     end,
 
-  ?PRINT_CALL(StreamedObject:write_data_stream(WriteDataFun, <<"text/plain">>, 1000)),
+  ?PRINT_CALL(cferl_object:write_data_stream(CloudFiles, StreamedObject, WriteDataFun, <<"text/plain">>, 1000)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Data can be streamed from the server to a receiving function"),
   ReadDataFun = fun(_Data) -> ok end,
-  ?PRINT_CALL(ok = StreamedObject:read_data_stream(ReadDataFun)),
+  ?PRINT_CALL(ok = cferl_object:read_data_stream(CloudFiles, StreamedObject, ReadDataFun)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Create all the directory elements for a particular object path"),
-  ?PRINT_CALL(ok = RefreshedContainer:ensure_dir(<<"photos/plants/fern.jpg">>)),
-  ?PRINT_CALL(true = RefreshedContainer:object_exists(<<"photos">>)),
-  ?PRINT_CALL(true = RefreshedContainer:object_exists(<<"photos/plants">>)),
+  ?PRINT_CALL(ok = cferl_container:ensure_dir(CloudFiles, RefreshedContainer, <<"photos/plants/fern.jpg">>)),
+  ?PRINT_CALL(true = cferl_container:object_exists(CloudFiles, RefreshedContainer, <<"photos">>)),
+  ?PRINT_CALL(true = cferl_container:object_exists(CloudFiles, RefreshedContainer, <<"photos/plants">>)),
   ?PRINT_CODE(""),
 
   % delete the streamed object
-  ok = RefreshedContainer:delete_object(<<"streamed.txt">>),
+  ok = cferl_container:delete_object(CloudFiles, RefreshedContainer, <<"streamed.txt">>),
   
   % delete the path elements
-  ok = RefreshedContainer:delete_object(<<"photos">>),
-  ok = RefreshedContainer:delete_object(<<"photos/plants">>),
+  ok = cferl_container:delete_object(CloudFiles, RefreshedContainer, <<"photos">>),
+  ok = cferl_container:delete_object(CloudFiles, RefreshedContainer, <<"photos/plants">>),
   
   % ensure log retention can be stopped
-  ok = RefreshedContainer:set_log_retention(false),
+  ok = cferl_container:set_log_retention(CloudFiles, RefreshedContainer, false),
   
   ?PRINT_CODE("# Make the container private"),
-  ?PRINT_CALL(ok = RefreshedContainer:make_private()),
+  ?PRINT_CALL(ok = cferl_container:make_private(CloudFiles, RefreshedContainer)),
   ?PRINT_CODE(""),
   
   ?PRINT_CODE("# Delete an existing container (must be empty)"),
-  ?PRINT_CALL(ok = RefreshedContainer:delete()),
+  ?PRINT_CALL(ok = cferl_container:delete(CloudFiles, RefreshedContainer)),
   ?PRINT_CODE(""),
   
   % ensure deleting missing container is properly handled
-  {error, not_found} = NewContainer:delete(),
+  {error, not_found} = cferl_container:delete(CloudFiles, NewContainer),
   
   ok.
   
